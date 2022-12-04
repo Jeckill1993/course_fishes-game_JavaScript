587e0eeb21a72729b88e6b70bf73f4bc947ca1ec
Jeckill1993 <evgeniyduplenko@gmail.com>
2022-12-04 06:54:17 +0000

    [main] init project

---
+++ js/script.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', () => {
+    const canvas = document.getElementById('canvas');
+    if (canvas) canvas.getContext('2d');
+
+    canvas.width = 1500;
+    canvas.heigh = 500;
+
+    class InputHandler {
+
+    }
+
+    class Projectile {
+
+    }
+
+    class Particle {
+
+    }
+
+    class Player {
+
+    }
+
+    class Enemy {
+
+    }
+
+    class Layer {
+
+    }
+
+    class Background {
+
+    }
+
+    class UI {
+
+    }
+
+    class Game {
+
+    }
+});
