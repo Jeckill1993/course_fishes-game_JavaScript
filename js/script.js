18bf3fe046ff9f6cd6ee0de585c9e17376965d79
Jeckill1993 <evgeniyduplenko@gmail.com>
2022-12-07 23:04:52 +0000

    add start animation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 window.addEventListener('load', () => {
     const canvas = document.getElementById('canvas');
-    if (canvas) canvas.getContext('2d');
+    const context = canvas ? canvas.getContext('2d') : null; // update the expression later with eslint
 
     canvas.width = 1500;
     canvas.heigh = 500;
 
     class InputHandler {
-
+        constructor(game) {
+            this.game = game;
+            window.addEventListener('keydown', (e) => {
+                console.log(e.key);
+            })
+        }
     }
 
     class Projectile {
@@ -18,6 +23,22 @@ window.addEventListener('load', () => {
     }
 
     class Player {
+        constructor(game) {
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 1;
+        }
+
+        update() {
+            this.y += this.speedY;
+        }
+
+        draw(context) {
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
 
     }
 
@@ -38,6 +59,30 @@ window.addEventListener('load', () => {
     }
 
     class Game {
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+            this.input = new InputHandler(this);
+        }
 
+        update() {
+            this.player.update();
+        }
+
+        draw(context) {
+            this.player.draw(context);
+        }
     }
+
+    const game = new Game(canvas.width, canvas.height);
+
+    function animate() {
+        context.clearRect(0, 0, canvas.width, canvas.heigh);
+        game.update();
+        game.draw(context);
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 });
